a9c705df2e46996d663800a1ba1960145a773445
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-15 15:24:37 -0500

    added response

--- https_example.js
+++ https_example.js
@@ -4,8 +4,9 @@ var options = {
   path: '/'
 };
 
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 };
 
 console.log("I'm about to make a request!");
